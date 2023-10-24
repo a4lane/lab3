712f1e7a51be99f6ff9cbca993ca160c6f7fc5c5
a4lane <a4lane@ucsd.edu>
2023-10-24 13:11:15 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -28,4 +28,13 @@ public class ArrayTests {
     int[] input2 = {4, 5, 6};
     assertArrayEquals(new int[]{6, 5, 4}, ArrayExamples.reversed(input2));
   }
+
+
+  @Test
+  public void testAverageWithoutLowest(){
+    double[] basecase = {9.5};
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(basecase), 0.001);
+    double[] multipleLowest = {8.0, 3.5, 3.5, 6.0, 10.0, 3.5};
+    assertEquals(8.0, ArrayExamples.averageWithoutLowest(multipleLowest), 0.001);
+  }
 }
